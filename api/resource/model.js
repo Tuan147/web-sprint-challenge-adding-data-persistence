b6f19d2aa328a5c848628d9b97e54a7b34e5a6fc
unknown <tuan5301@gmail.com>
2022-02-18 12:05:14 -0500

    added in all api portion and now working on passing test

--- api/resource/model.js
+++ api/resource/model.js
@@ -1,1 +1,17 @@
 // build your `Resource` model here
+const db = require('../../data/dbConfig');
+const { resource } = require('../server');
+
+const getAll = () => {
+    return db('resources')
+};
+
+const create = async (resource) => {
+    const [id] = await db('resources').insert(resource)
+    return db('resources').where('resource_id', id).first()
+};
+
+module.exports = {
+    getAll,
+    create
+}
--- api/task/router.js
+++ api/task/router.js
@@ -1,1 +1,32 @@
 // build your `/api/tasks` router here
+const express = require('express');
+
+const Task = require('./model');
+
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+    Task.getAll()
+        .then(task => {
+            res.status(200).json(task)
+        })
+        .catch(next)
+});
+
+router.post('/', async (req, res, next) => {
+    try {
+        const newTask = await Task.create(req.body)
+        res.status(201).json({
+            task_id: newTask.task_id,
+            task_description: newTask.task_description,
+            task_notes: newTask.task_notes,
+            task_completed: newTask.task_completed === 0 ? false : true,
+            project_id: newTask.project_id
+        })
+    }
+    catch (e) {
+        next(e)
+    }
+})
+
+module.exports = router;
